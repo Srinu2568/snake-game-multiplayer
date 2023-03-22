1a11b35374636ed40b586fe080a584713a0ff283
srinivas <mnvsrinivas1@gmail.com>
2023-03-22 13:05:20 +0530

    Animate snake with sprites

--- frontend/index.js
+++ frontend/index.js
@@ -80,12 +80,120 @@ function paintGame(state) {
     paintPlayer(state.players[1], size, 'blue');
 }
 
+function loadImages(imagefiles) {
+    // Initialize variables
+    loadcount = 0;
+    loadtotal = imagefiles.length;
+    preloaded = false;
+    
+    // Load the images
+    var loadedimages = [];
+    for (var i=0; i<imagefiles.length; i++) {
+        // Create the image object
+        var image = new Image();
+        
+        // Add onload event handler
+        image.onload = function () {
+            loadcount++;
+            if (loadcount == loadtotal) {
+                // Done loading
+                preloaded = true;
+            }
+        };
+        
+        // Set the source url of the image
+        image.src = imagefiles[i];
+        
+        // Save to the image array
+        loadedimages[i] = image;
+    }
+    
+    // Return an array of images
+    return loadedimages;
+}
+
 function paintPlayer(playerState, size, colour) {
     const snake = playerState.snake;
+    // Load images
+    images = loadImages(["snake-graphics.png"]);
+    tileimage = images[0];
+    // ctx.fillStyle = colour;
+    // for (cell of snake) {
+    //     ctx.fillRect(cell.x * size, cell.y * size, size, size);
+    // }
 
-    ctx.fillStyle = colour;
-    for (cell of snake) {
-        ctx.fillRect(cell.x * size, cell.y * size, size, size);
+    // Loop over every snake segment
+    for (var i=0; i<snake.length; i++) {
+        var segment = snake[i];
+        var segx = segment.x;
+        var segy = segment.y;
+        var tilex = segx*size;
+        var tiley = segy*size;
+        
+        // Sprite column and row that gets calculated
+        var tx = 0;
+        var ty = 0;
+        
+        if (i == 0) {
+            // Head; Determine the correct image
+            var nseg = snake[i+1]; // Next segment
+            if (segy < nseg.y) {
+                // Up
+                tx = 4; ty = 3;
+            } else if (segx > nseg.x) {
+                // Right
+                tx = 3; ty = 3;
+            } else if (segy > nseg.y) {
+                // Down
+                tx = 3; ty = 2;
+            } else if (segx < nseg.x) {
+                // Left
+                tx = 4; ty = 2;
+            }
+        } else if (i == snake.length-1) {
+            // Tail; Determine the correct image
+            var pseg = snake[i-1]; // Prev segment
+            if (pseg.y < segy) {
+                // Up
+                tx = 4; ty = 1;
+            } else if (pseg.x > segx) {
+                // Right
+                tx = 3; ty = 1;
+            } else if (pseg.y > segy) {
+                // Down
+                tx = 3; ty = 0;
+            } else if (pseg.x < segx) {
+                // Left
+                tx = 4; ty = 0;
+            }
+        } else {
+            // Body; Determine the correct image
+            var pseg = snake[i-1]; // Previous segment
+            var nseg = snake[i+1]; // Next segment
+            if (pseg.x < segx && nseg.x > segx || nseg.x < segx && pseg.x > segx) {
+                // Horizontal Left-Right
+                tx = 1; ty = 0;
+            } else if (pseg.x < segx && nseg.y > segy || nseg.x < segx && pseg.y > segy) {
+                // Angle Left-Down
+                tx = 2; ty = 0;
+            } else if (pseg.y < segy && nseg.y > segy || nseg.y < segy && pseg.y > segy) {
+                // Vertical Up-Down
+                tx = 2; ty = 1;
+            } else if (pseg.y < segy && nseg.x < segx || nseg.y < segy && pseg.x < segx) {
+                // Angle Top-Left
+                tx = 2; ty = 2;
+            } else if (pseg.x > segx && nseg.y < segy || nseg.x > segx && pseg.y < segy) {
+                // Angle Right-Up
+                tx = 0; ty = 1;
+            } else if (pseg.y > segy && nseg.x > segx || nseg.y > segy && pseg.x > segx) {
+                // Angle Down-Right
+                tx = 0; ty = 0;
+            }
+        }
+        
+        // Draw the image of the snake part
+        ctx.drawImage(tileimage, tx*64, ty*64, 64, 64, tilex, tiley,
+                            size, size);
     }
 }
 
